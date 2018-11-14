140d1c0cce7dd6995a616c6371791c82e113e514
Ghassanooooo <ghassanaldarwish@yahoo.com>
2018-11-14 12:31:45 +0100

    added input field in common folder

---
+++ src/components/common/input/input.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  }
+});
+
+class OutlinedTextFields extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <TextField
+      {...this.props}
+        label={`${this.props.label}`}
+        className={classes.textField}
+        type={`${this.props.type || "text"}`}
+        margin="normal"
+        variant="outlined"
+        onChange={this.props.onChange}
+        value={this.props.value}
+      />
+    );
+  }
+}
+
+export default withStyles(styles)(OutlinedTextFields);
