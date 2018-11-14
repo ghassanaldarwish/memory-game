dec773a43ae83bdd284d23d09ae3f5a36565ca17
Jürgen <behighspy@yahoo.de>
2018-11-14 16:30:30 +0100

    home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './home.css';
+import homer from "../../assets/homer.jpg";
 
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <p>Home</p>
+      <div className="Home bg-overlay container-fluid">
+        <div className="m-auto">
+          <h1>How good is your memory ???</h1>
+
+          <img src={homer} alt="oh noo"/>
+          <h2>Create your own interactive memory game</h2>
+          <ul className="ulist">
+            <li>upload your own images with just a few clicks</li>
+            <li>choose the size of the game</li>
+            <li>invite your friends to play</li>
+          </ul>
+
+          <p>Give it a try push START</p>
+        </div>
+
       </div>
     );
   }
