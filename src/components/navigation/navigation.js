e6c247889c27efd7f75d7f4f64b97020a46a6f37
Ghassanooooo <ghassanaldarwish@yahoo.com>
2018-11-14 13:28:34 +0100

    added the nav bar

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,9 +4,60 @@ import './navigation.css';
 class Navigation extends Component {
   render() {
     return (
-      <div className="Navigation">
-        <p>Navigation</p>
-      </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="/">
+            Memory Game
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+
+          <div class="collapse navbar-collapse d-flex flex-column-reverse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Avatar
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="/">
+                    Profile
+                  </a>
+                  <a class="dropdown-item" href="/">
+                    Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
