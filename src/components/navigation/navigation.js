dd9eee1c82db1302d7480dcb2034b986bb34fd0f
Ghassanooooo <ghassanaldarwish@yahoo.com>
2018-11-14 14:01:53 +0100

    style the nav bar flex-flow:row-reverse

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import './navigation.css';
+import React, { Component } from "react";
+import "./navigation.css";
 
 class Navigation extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand" href="/">
-            Memory Game
+      <nav className="navbar navbar-expand-lg bg-secondary navbar-light ">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Navbar
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon" />
+            <span className="navbar-toggler-icon" />
           </button>
-
-          <div class="collapse navbar-collapse d-flex flex-column-reverse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="/">
-                  Home <span class="sr-only">(current)</span>
+          <div className="collapse navbar-collapse NavigationFlexFix" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Home <span className="sr-only">(current)</span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  Link
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Sign in
                 </a>
               </li>
-              <li class="nav-item dropdown">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                Sign up
+                </a>
+              </li>
+              <li className="nav-item dropdown">
                 <a
-                  class="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle"
                   href="/"
-                  id="navbarDropdown"
+                  id="navbarDropdownMenuLink"
                   role="button"
                   data-toggle="dropdown"
                   aria-haspopup="true"
@@ -45,11 +49,14 @@ class Navigation extends Component {
                 >
                   Avatar
                 </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="/">
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <a className="dropdown-item" href="/">
                     Profile
                   </a>
-                  <a class="dropdown-item" href="/">
+                  <a className="dropdown-item" href="/">
                     Logout
                   </a>
                 </div>
