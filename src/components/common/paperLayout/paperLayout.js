672868e55ebd0d0ee159cb43e7b9e4cdc26e8123
Ghassanooooo <ghassanaldarwish@yahoo.com>
2018-11-15 12:59:00 +0100

    added the route to render the momponents with routs

--- src/components/common/paperLayout/paperLayout.js
+++ src/components/common/paperLayout/paperLayout.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-  },
+    paddingBottom: theme.spacing.unit * 2
+  }
 });
 
 function paperLayout(props) {
   const { classes } = props;
 
   return (
-   
+    <div>
       <Paper className={classes.root} elevation={1}>
-       {props.children}
+        {props.children}
       </Paper>
-   
+    </div>
   );
 }
-
-
 
 export default withStyles(styles)(paperLayout);
