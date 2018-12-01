702aa1ef5a4d3864d6b281f56bcf23eb259eb944
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-12-01 18:34:33 +0100

    Updates

---
+++ precache-manifest.ef7247180788b47e206d5bf7dd9e8753.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45a8ad6ad78cf626898cfdaab2b273b",
+    "url": "/memory-game/static/media/textur.e45a8ad6.jpg"
+  },
+  {
+    "revision": "3121dacc0c94b55d3b0fb1230e495489",
+    "url": "/memory-game/static/media/mem-home.3121dacc.png"
+  },
+  {
+    "revision": "268e89173e354128d060c8b204f138c5",
+    "url": "/memory-game/static/media/homer.268e8917.jpg"
+  },
+  {
+    "revision": "529e0b471a0a7a8094bd058cd7f0acea",
+    "url": "/memory-game/static/media/back.529e0b47.jpg"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "acdea1ebb74152697843",
+    "url": "/memory-game/static/js/main.acdea1eb.chunk.js"
+  },
+  {
+    "revision": "3ca9ed802783a1264c40",
+    "url": "/memory-game/static/js/1.3ca9ed80.chunk.js"
+  },
+  {
+    "revision": "acdea1ebb74152697843",
+    "url": "/memory-game/static/css/main.0eea1232.chunk.css"
+  },
+  {
+    "revision": "912ee3fe3b7794852074d827ff5bf5b0",
+    "url": "/memory-game/index.html"
+  }
+];
